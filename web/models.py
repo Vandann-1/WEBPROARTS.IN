ca1721ea256fd1df8776fc390989f94a8d6e0f9c
unknown <vandanparmar6041@gmail.com>
2026-04-02 13:30:39 +0530

    ui

--- web/models.py
+++ web/models.py
@@ -3,10 +3,11 @@ from django.db import models
 # Create your models here.
 
 class formdata(models.Model):
-    name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
     email = models.EmailField()
     subject = models.CharField(max_length=200)
     message = models.TextField()
 
     def __str__(self):
-        return self.name
+        return f"{self.first_name} {self.last_name}"
--- webproarts/settings.py
+++ webproarts/settings.py
@@ -58,8 +58,8 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = ""
-EMAIL_HOST_PASSWORD = ""
+EMAIL_HOST_USER = "webproarts@gmail.com"
+EMAIL_HOST_PASSWORD = "sleh wioe xfgw nwxl"
 
 DEFAULT_FROM_EMAIL = "Slotify <coderspamm13@gmail.com>"
 
