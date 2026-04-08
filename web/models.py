44e1d2c35b5cf8fc6c6e58eb4a53da93be11a04c
unknown <vandanparmar6041@gmail.com>
2026-04-08 10:19:57 +0530

    ui oogle review

--- web/models.py
+++ web/models.py
@@ -30,3 +30,11 @@ class BlogPost(models.Model):
     def __str__(self):
         return self.title
     
+class googlereview(models.Model):
+    name = models.CharField(max_length=100)
+    review = models.TextField()
+    rating = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.rating} Stars"    
