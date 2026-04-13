b1a9ef69532d58e950e4ff02f73a7f79f9ee7a41
unknown <vandanparmar6041@gmail.com>
2026-04-13 11:24:49 +0530

    dDD

--- webproarts/sitemap.py
+++ webproarts/sitemap.py
@@ -1,25 +1,29 @@
 from django.contrib.sitemaps import Sitemap
 from django.urls import reverse
+from web.models import *  # Import your Blog model
 
 class StaticSitemap(Sitemap):
     changefreq = "weekly"
     priority = 0.9
 
     def items(self):
+        # Remove 'blog_detail' from here because it needs an ID/Slug
         return [
-            'home',
-            'about',
-            'contact.html',
-            'blogs',
-            'digital_marketing',
-            'web_design',
-            'video_editing',
-            'social_media',
-            'privacy_policy',
-            'terms_of_service',
-            'graphic_design',
-            'blog_detail',
+            'home', 'about', 'contact.html', 'blogs',
+            'digital_marketing', 'web_design', 'video_editing',
+            'social_media', 'privacy_policy', 'terms_of_service', 'graphic_design',
         ]
 
     def location(self, item):
         return reverse(item)
+
+# Add this to include your actual blog posts!
+class BlogSitemap(Sitemap):
+    changefreq = "daily"
+    priority = 0.7
+
+    def items(self):
+        return BlogPost.objects.all() # Or Post.objects.filter(status='published')
+
+    def lastmod(self, obj):
+        return obj.updated_at # Helps Google know when you edited a post
--- webproarts/urls.py
+++ webproarts/urls.py
@@ -1,21 +1,24 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.sitemaps.views import sitemap
-from webproarts.sitemap import StaticSitemap
+from webproarts.sitemap import * # Ensure BlogSitemap is ready
 from django.views.generic import TemplateView
 from django.conf import settings
 
+# Include both static pages and blog posts for full SEO coverage
 sitemaps = {
     'static': StaticSitemap,
+    'blog': BlogSitemap, 
 }
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    # Sitemap
-    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='sitemap'),
+    # Sitemap - This generates the XML for Google
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, 
+         name='django.contrib.sitemaps.views.sitemap'),
 
-    # Robots.txt
+    # Robots.txt - Served as a template to use your SITE_URL variable
     path('robots.txt', TemplateView.as_view(
         template_name="robots.txt",
         content_type="text/plain",
